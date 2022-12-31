c2a96f5404bce9639ef9146cbacc45c24d4efedf
code-cp <moshan.nus@gmail.com>
2022-12-31 14:53:59 +0800

    first commit

---
+++ transformer/readme.md
@@ -0,0 +1,3 @@
+## references 
+
+- https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial6/Transformers_and_MHAttention.html
---
+++ transformer/seqtoseq/mydata.py
@@ -0,0 +1,28 @@
+import torch 
+import from torch.utils.data as data 
+
+class ReverseDataset(data.Dataset): 
+    def __init__(self, num_categories, seq_len, size): 
+        super().__init__()
+        self.num_categories = num_categories 
+        self.seq_len = seq_len 
+        self.size = size 
+
+        self.data = torch.randint(self.num_categories, size=(self.size, self.seq_len))
+
+    def __len__(self): 
+        return self.size 
+
+    def __get__item(self, idx): 
+        inp_data = self.data[idx]
+        labels = torch.flip(inp_data, dims=(0,))
+        return inp_data, labels 
+        
+if __name__ == "__main__": 
+    dataset = partial(ReverseDataset, 10, 16)
+    train_loader = data.DataLoader(dataset(50000), batch_size=128,shuffle=True,drop_last=True,pin_memory=True)
+    # val_loader = data.DataLoader(dataset(1000), batch_size=128)
+    # test_loader = data.DataLoader(dataset(10000), batch_size=128)
+    inp_data, labels = train_loader.dataset[0]
+    print(f"input {inp_data}")
+    print(f"labels {labels}")
